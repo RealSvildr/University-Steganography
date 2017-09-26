a231053d37e3386d849c6188cbfdced0172228ca
Svildr <svildr@gmail.com>
2017-09-26 05:11:21 -0300

    Add files via upload

---
+++ Steganography/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Steganography {
+    partial class Steganography {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Steganography));
+            this.txt_Container = new System.Windows.Forms.TextBox();
+            this.btn_decrypt = new System.Windows.Forms.Button();
+            this.txt_Content = new System.Windows.Forms.TextBox();
+            this.btn_encrypt = new System.Windows.Forms.Button();
+            this.btn_Container = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_Content = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ckb_Compress = new System.Windows.Forms.CheckBox();
+            this.txt_RGBOrder = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_Container
+            // 
+            this.txt_Container.Location = new System.Drawing.Point(11, 24);
+            this.txt_Container.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Container.Name = "txt_Container";
+            this.txt_Container.ReadOnly = true;
+            this.txt_Container.Size = new System.Drawing.Size(194, 20);
+            this.txt_Container.TabIndex = 0;
+            this.txt_Container.Click += new System.EventHandler(this.txt_Container_Click);
+            // 
+            // btn_decrypt
+            // 
+            this.btn_decrypt.Location = new System.Drawing.Point(57, 130);
+            this.btn_decrypt.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_decrypt.Name = "btn_decrypt";
+            this.btn_decrypt.Size = new System.Drawing.Size(65, 23);
+            this.btn_decrypt.TabIndex = 1;
+            this.btn_decrypt.Text = "Decrypt";
+            this.btn_decrypt.UseVisualStyleBackColor = true;
+            this.btn_decrypt.Click += new System.EventHandler(this.btn_Decrypt_Click);
+            // 
+            // txt_Content
+            // 
+            this.txt_Content.Location = new System.Drawing.Point(10, 71);
+            this.txt_Content.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_Content.Name = "txt_Content";
+            this.txt_Content.ReadOnly = true;
+            this.txt_Content.Size = new System.Drawing.Size(195, 20);
+            this.txt_Content.TabIndex = 2;
+            this.txt_Content.Click += new System.EventHandler(this.txt_Content_Click);
+            // 
+            // btn_encrypt
+            // 
+            this.btn_encrypt.Location = new System.Drawing.Point(127, 130);
+            this.btn_encrypt.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_encrypt.Name = "btn_encrypt";
+            this.btn_encrypt.Size = new System.Drawing.Size(65, 23);
+            this.btn_encrypt.TabIndex = 3;
+            this.btn_encrypt.Text = "Encrypt";
+            this.btn_encrypt.UseVisualStyleBackColor = true;
+            this.btn_encrypt.Click += new System.EventHandler(this.btn_Encrypt_Click);
+            // 
+            // btn_Container
+            // 
+            this.btn_Container.Image = ((System.Drawing.Image)(resources.GetObject("btn_Container.Image")));
+            this.btn_Container.Location = new System.Drawing.Point(210, 19);
+            this.btn_Container.Name = "btn_Container";
+            this.btn_Container.Size = new System.Drawing.Size(30, 28);
+            this.btn_Container.TabIndex = 4;
+            this.btn_Container.UseVisualStyleBackColor = true;
+            this.btn_Container.Click += new System.EventHandler(this.btn_Container_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_Content
+            // 
+            this.btn_Content.Image = ((System.Drawing.Image)(resources.GetObject("btn_Content.Image")));
+            this.btn_Content.Location = new System.Drawing.Point(209, 66);
+            this.btn_Content.Name = "btn_Content";
+            this.btn_Content.Size = new System.Drawing.Size(30, 28);
+            this.btn_Content.TabIndex = 5;
+            this.btn_Content.UseVisualStyleBackColor = true;
+            this.btn_Content.Click += new System.EventHandler(this.btn_Content_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Image Container";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Content";
+            // 
+            // ckb_Compress
+            // 
+            this.ckb_Compress.AutoSize = true;
+            this.ckb_Compress.Location = new System.Drawing.Point(10, 96);
+            this.ckb_Compress.Name = "ckb_Compress";
+            this.ckb_Compress.Size = new System.Drawing.Size(149, 17);
+            this.ckb_Compress.TabIndex = 8;
+            this.ckb_Compress.Text = "Most Compressed Method";
+            this.ckb_Compress.UseVisualStyleBackColor = true;
+            this.ckb_Compress.CheckedChanged += new System.EventHandler(this.ckb_Compress_CheckedChanged);
+            // 
+            // txt_RGBOrder
+            // 
+            this.txt_RGBOrder.Location = new System.Drawing.Point(183, 100);
+            this.txt_RGBOrder.MaxLength = 3;
+            this.txt_RGBOrder.Name = "txt_RGBOrder";
+            this.txt_RGBOrder.Size = new System.Drawing.Size(57, 20);
+            this.txt_RGBOrder.TabIndex = 9;
+            this.txt_RGBOrder.Text = "RGB";
+            this.txt_RGBOrder.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_RGBOrder_KeyPress);
+            // 
+            // Steganography
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(249, 164);
+            this.Controls.Add(this.txt_RGBOrder);
+            this.Controls.Add(this.ckb_Compress);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_Content);
+            this.Controls.Add(this.btn_Container);
+            this.Controls.Add(this.btn_encrypt);
+            this.Controls.Add(this.txt_Content);
+            this.Controls.Add(this.btn_decrypt);
+            this.Controls.Add(this.txt_Container);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Steganography";
+            this.Text = "Steganography";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Container;
+        private System.Windows.Forms.Button btn_decrypt;
+        private System.Windows.Forms.TextBox txt_Content;
+        private System.Windows.Forms.Button btn_encrypt;
+        private System.Windows.Forms.Button btn_Container;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_Content;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox ckb_Compress;
+        private System.Windows.Forms.TextBox txt_RGBOrder;
+    }
+}
+
---
+++ Steganography/Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+
+// TODO: For now is only Encrypting PNG and BMP, when it saves jpg the image quality is lowered and fuck up all binary data
+namespace Steganography {
+    public partial class Steganography : Form {
+        public Steganography() {
+            InitializeComponent();
+            this.MaximizeBox = false;
+            //this.MinimizeBox = false;
+        }
+
+        private void btn_Decrypt_Click(object sender, EventArgs e) {
+            if (txt_Container.Text == "") {
+                MessageBox.Show("Select an image.");
+                return;
+            }
+
+            try {
+                string imageFile = txt_Container.Text;
+                StringBuilder stBuilder = new StringBuilder();
+                Bitmap image = new Bitmap(imageFile);
+                int x = 0, y = 0;
+                Color color;
+
+                if (ckb_Compress.Checked) { // Minimum Significative bit
+                    for (x = 0; x < image.Width; x++) {
+                        for (y = 0; y < image.Height; y++) {
+                            color = image.GetPixel(x, y);
+                            stBuilder.Append(color.ToArgb() % 2);
+                        }
+                    }
+                } else { // Minimum significative bit inside the RGB
+                    if (txt_RGBOrder.TextLength != 3) {
+                        MessageBox.Show("You have to inform the RGB order to decrypt.");
+                        return;
+                    }
+
+                    char elem_1 = txt_RGBOrder.Text[0],
+                         elem_2 = txt_RGBOrder.Text[1],
+                         elem_3 = txt_RGBOrder.Text[2];
+
+                    for (x = 0; x < image.Width; x++) {
+                        for (y = 0; y < image.Height; y++) {
+                            color = image.GetPixel(x, y);
+
+                            stBuilder.Append(getByte(color, elem_1) % 2);
+                            stBuilder.Append(getByte(color, elem_2) % 2);
+                            stBuilder.Append(getByte(color, elem_3) % 2);
+                        }
+                    }
+                }
+
+                //////////////////////////////////////////////////
+
+                while (stBuilder.Length % 8 != 0) {
+                    stBuilder.Append("0");
+                }
+
+                List<byte> archive = new List<byte>();
+
+                archive = getBytes(stBuilder.ToString());
+
+                while (archive.Last() == 0x00) {
+                    archive.RemoveAt(archive.Count - 1);
+                }
+
+                //1111 1111 -- END
+                if (archive.Last() == 0xFF) {
+                    archive.RemoveAt(archive.Count - 1);
+
+                    string fileType = "";
+                    List<byte> byteFileType = new List<byte>();
+
+                    x = 0;
+                    while (archive.Last() != 0xFF) {
+                        byteFileType.Add(archive.Last());
+                        archive.RemoveAt(archive.Count - 1);
+                        x++;
+                    }
+                    archive.RemoveAt(archive.Count - 1);
+
+                    //gpj --> jpg
+                    byteFileType = Enumerable.Reverse(byteFileType).ToList();
+                    fileType = Encoding.UTF8.GetString(byteFileType.ToArray());
+
+                    using (FileStream file = File.Create(imageFile + "_decrypted." + fileType)) {
+                        file.Write(archive.ToArray(), 0, archive.Count);
+                        file.Close();
+                    }
+                } else {
+                    MessageBox.Show("This image doesn't contain a cryptographed archive.");
+                }
+
+
+            } catch (Exception ex) {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+        private void btn_Encrypt_Click(object sender, EventArgs e) {
+            if (txt_Container.Text == "") {
+                MessageBox.Show("Select an image.");
+                return;
+            }
+
+            if (txt_Content.Text == "") {
+                MessageBox.Show("Select an archive.");
+                return;
+            }
+
+            try {
+                string imageFile = txt_Container.Text;
+                string archiveType = txt_Content.Text.Split('.').Last();
+                int x = 0, y = 0, z = 0;
+
+                List<byte> listBytes = new List<byte>();
+                List<int> listBit = new List<int>();
+                Bitmap image = new Bitmap(imageFile);
+                Bitmap editImage = image.Clone(new Rectangle(0, 0, image.Width, image.Height), System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                //image.Dispose();
+                Color color;
+
+                using (FileStream fStream = File.OpenRead(txt_Content.Text)) {
+                    var Byte = 0;
+
+                    while ((Byte = fStream.ReadByte()) > -1) {
+                        listBytes.Add(Convert.ToByte(Byte));
+                    }
+
+                    fStream.Dispose();
+                }
+
+                /* Validation */
+                listBytes.Add(0xFF);
+                /* Archive Type */
+                for (x = 0; x < archiveType.Length; x++) {
+                    listBytes.Add(Convert.ToByte(archiveType[x]));
+                }
+                /* END Archive Type */
+                listBytes.Add(0xFF);
+                /* END Validation */
+
+                foreach (var Byte in listBytes) {
+                    string stringByte = string.Format("{0,8}", Convert.ToString(Byte, 2)).Replace(' ', '0');
+                    foreach (var bit in stringByte) {
+                        listBit.Add(bit == 49 ? 1 : 0);
+                    }
+                }
+
+                long bitNeeded = listBit.Count();
+
+                //////////////////
+                if (ckb_Compress.Checked) { // Minimum Significative bit
+                    int tempColor = 0;
+
+                    if ((image.Width * image.Height) < bitNeeded) {
+                        MessageBox.Show("The image is too small to insert the archive.");
+                        return;
+                    }
+
+                    for (x = 0; x < editImage.Width; x++) {
+                        for (y = 0; y < editImage.Height; y++) {
+                            color = editImage.GetPixel(x, y);
+                            tempColor = color.ToArgb();
+
+                            if (listBit.Count() > z) {
+                                if (tempColor % 2 != listBit[z]) {
+                                    tempColor = tempColor > 0 ? tempColor - 1 : tempColor + 1;
+                                    editImage.SetPixel(x, y, Color.FromArgb(tempColor));
+                                }
+                                z++;
+                            } else {
+                                if (tempColor % 2 != 0) {
+                                    tempColor = tempColor > 0 ? tempColor - 1 : tempColor + 1;
+                                    editImage.SetPixel(x, y, Color.FromArgb(tempColor));
+                                }
+                            }
+                        }
+                    }
+                } else { // Minimum significative bit inside the RGB
+                    int tempColorA = 0, tempColor1 = 0, tempColor2 = 0, tempColor3 = 0;
+
+                    if ((image.Width * image.Height * 3) < bitNeeded) {
+                        MessageBox.Show("The image is too small to insert the archive.");
+                        return;
+                    }
+
+                    if (txt_RGBOrder.TextLength != 3) {
+                        MessageBox.Show("You have to inform the RGB order to decrypt.");
+                        return;
+                    }
+
+                    char elem_1 = txt_RGBOrder.Text[0],
+                         elem_2 = txt_RGBOrder.Text[1],
+                         elem_3 = txt_RGBOrder.Text[2];
+
+                    for (x = 0; x < editImage.Width; x++) {
+                        for (y = 0; y < editImage.Height; y++) {
+                            color = editImage.GetPixel(x, y);
+                            tempColorA = color.A;
+
+                            tempColor1 = getByte(color, elem_1);
+                            if (listBit.Count() > z) {
+                                if (tempColor1 % 2 != listBit[z]) {
+                                    tempColor1 = tempColor1 > 0 ? tempColor1 - 1 : tempColor1 + 1;
+                                }
+                                z++;
+                            } else {
+                                if (tempColor1 % 2 != 0) {
+                                    tempColor1 = tempColor1 > 0 ? tempColor1 - 1 : tempColor1 + 1;
+                                }
+                            }
+
+                            tempColor2 = getByte(color, elem_2);
+                            if (listBit.Count() > z) {
+                                if (tempColor2 % 2 != listBit[z]) {
+                                    tempColor2 = tempColor2 > 0 ? tempColor2 - 1 : tempColor2 + 1;
+                                }
+                                z++;
+                            } else {
+                                if (tempColor2 % 2 != 0) {
+                                    tempColor2 = tempColor2 > 0 ? tempColor2 - 1 : tempColor2 + 1;
+                                }
+                            }
+
+                            tempColor3 = getByte(color, elem_3);
+                            if (listBit.Count() > z) {
+                                if (tempColor3 % 2 != listBit[z]) {
+                                    tempColor3 = tempColor3 > 0 ? tempColor3 - 1 : tempColor3 + 1;
+                                }
+                                z++;
+                            } else {
+                                if (tempColor3 % 2 != 0) {
+                                    tempColor3 = tempColor3 > 0 ? tempColor3 - 1 : tempColor3 + 1;
+                                }
+                            }
+
+                            Color tempColor = getColor(elem_1, elem_2, elem_3, tempColorA, tempColor1, tempColor2, tempColor3);
+
+                            if (color != tempColor) {
+                                editImage.SetPixel(x, y, tempColor);
+                            }
+                        }
+                    }
+
+
+                    editImage.Save("imageEncrypted_" + DateTime.Now.ToString("yyyyMMddHHmm") + "." + archiveType);
+                    image.Dispose();
+                    editImage.Dispose();
+
+                    MessageBox.Show("Archive have been encryptographed.");
+                }
+            } catch (Exception ex) {
+                MessageBox.Show("An error occurred: " + ex.Message);
+            }
+        }
+
+        /////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void txt_Container_Click(object sender, EventArgs e) {
+            txt_Container.Text = getFile();
+        }
+        private void btn_Container_Click(object sender, EventArgs e) {
+            txt_Container.Text = getFile();
+        }
+        private void txt_Content_Click(object sender, EventArgs e) {
+            txt_Content.Text = getFile();
+        }
+        private void btn_Content_Click(object sender, EventArgs e) {
+            txt_Content.Text = getFile();
+        }
+
+        private void ckb_Compress_CheckedChanged(object sender, EventArgs e) {
+            txt_RGBOrder.Visible = !ckb_Compress.Checked;
+        }
+        private void txt_RGBOrder_KeyPress(object sender, KeyPressEventArgs e) {
+            var sKeys = "RGB\b";
+            var key = e.KeyChar.ToString().ToUpper();
+
+            if (!sKeys.Contains(key) || txt_RGBOrder.Text.ToUpper().Contains(key)) {
+                e.Handled = true;
+            }
+        }
+
+        private string getFile() {
+            openFileDialog1.Reset();
+            openFileDialog1.Multiselect = false;
+            openFileDialog1.Filter = "Image Files (*.bmp, *.jpg)|*.bmp;*.jpg";
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK) // Test result.
+            {
+                return openFileDialog1.FileName;
+            }
+
+            return null;
+        }
+        private int getByte(Color color, char rgb) {
+            switch (rgb) {
+                case 'R': return Convert.ToInt32(color.R);
+                case 'G': return Convert.ToInt32(color.G);
+                case 'B': return Convert.ToInt32(color.B);
+            }
+
+            return 0;
+        }
+        private List<byte> getBytes(string bitString) {
+            return Enumerable.Range(0, bitString.Length / 8).
+                Select(pos => Convert.ToByte(
+                    bitString.Substring(pos * 8, 8),
+                    2)
+                ).ToList();
+        }
+        private Color getColor(char rgb1, char rgb2, char rgb3, int colorA, int color1, int color2, int color3) {
+            int colorR = 0, colorG = 0, colorB = 0;
+
+            switch (rgb1) {
+                case 'R': colorR = color1; break;
+                case 'G': colorG = color1; break;
+                case 'B': colorB = color1; break;
+            }
+
+            switch (rgb2) {
+                case 'R': colorR = color2; break;
+                case 'G': colorG = color2; break;
+                case 'B': colorB = color2; break;
+            }
+
+            switch (rgb3) {
+                case 'R': colorR = color3; break;
+                case 'G': colorG = color3; break;
+                case 'B': colorB = color3; break;
+            }
+
+            return Color.FromArgb(colorA, colorR, colorG, colorB);
+
+        }
+        private int[] getBytes(StringBuilder stBuilder) {
+            return stBuilder.ToString().Select(p => Convert.ToInt32(p)).ToArray();
+        }
+    }
+}
